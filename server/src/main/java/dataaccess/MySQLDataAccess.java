b4748addb6546d85b8ea98ca1d170a44703822eb
aiden carter <rectar25@gmail.com>
2026-03-10 23:20:09 -0600

    unversioned files?

---
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -0,0 +1,94 @@
+package dataaccess;
+
+import model.*;
+import com.google.gson.Gson;
+import java.sql.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MySQLDataAccess implements DataAccess{
+    public MySQLDataAccess() throws DataAccessException{
+        configureDatabase();
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData createUser(RegisterRequest registerRequest) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public GameData createGame(String gameName) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public GameData updateGame(GameData game) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public AuthData createAuth(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String username) throws DataAccessException {
+
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+                CREATE TABLE IF NOT EXISTS  pet (
+                  `id` int NOT NULL AUTO_INCREMENT,
+                  `name` varchar(256) NOT NULL,
+                  `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+                  `json` TEXT DEFAULT NULL,
+                  PRIMARY KEY (`id`),
+                  INDEX(type),
+                  INDEX(name)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+                """
+    };
+
+}
