19aa18c2439f27679bee783a118a8bb9aedeec67
aiden carter <rectar25@gmail.com>
2026-04-09 16:38:03 -0600

    more WebsocketServer

--- server/src/main/java/server/WebsocketServer.java
+++ server/src/main/java/server/WebsocketServer.java
@@ -19,6 +19,40 @@ import websocket.messages.NotificationMessage;
 public class WebsocketServer {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final GameService gameService;
+    public WebsocketServer(Javalin server, GameService gameService) {
+        this.gameService = gameService;
+        server.ws("/ws", ws -> {
+            ws.onConnect(this:: websocketConnect);
+            ws.onMessage(this::websocketMessage);
+            ws.onClose(this:: websocketClose);
+        });
+    }
+
+    private void websocketClose(WsCloseContext wsCloseContext) {
+    }
+
+    private void websocketMessage(WsMessageContext ctx) {
+        try{
+            var command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+            switch(command.getCommandType()){
+                case LEAVE -> leaveGame(ctx,command);
+                case RESIGN -> resignGame(command);
+                case MAKE_MOVE -> makeMove(ctx, new Gson().fromJson(ctx.message(), MakeMoveCommand.class));
+                case CONNECT -> connectGame(ctx, command);
+            }
+        }
+        catch(Exception ex){
+            var error = new ErrorMessage(ex.getMessage());
+            ctx.send(error.toString());
+        }
+    }
+
+    private void websocketConnect(WsMessageContext ctx) {
+        ctx.enableAutomaticPings();
+        System.out.println("websocket connection established");
+    }
+
 
 }
 
--- shared/src/main/java/websocket/messages/ErrorMessage.java
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -1,4 +1,20 @@
 package websocket.messages;
 
 public class ErrorMessage {
+    private String message;
+    public ErrorMessage(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorMessage{" +
+                "message='" + message + '\'' +
+                '}';
+    }
 }
